56d35e5d9e76c5806fc6867802150161e4c8d3bd
Connorseds2002 <54939589+Connorseds2002@users.noreply.github.com>
2020-07-30 01:36:15 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogonComponent } from './logon/logon.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DiscordComponent } from './discord/discord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogonComponent,
+    SignupComponent,
+    MembersComponent,
+    AboutUsComponent,
+    DiscordComponent
   ],
   imports: [
     BrowserModule,
