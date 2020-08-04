d50d249ab436d024267410829e8242f214599f57
Connorseds2002 <54939589+Connorseds2002@users.noreply.github.com>
2020-08-04 02:56:38 +0100

    Delete config.ts

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const token: string = "NzM5MjA4Mjg5OTYyNjg4NTQ1.XyXHXg.JjA95wvQ-7kUfQPnerJnQFr1W-A";
-export const prefix: string = "!";
-export const owners: string[] = ["391319956673003524"];
-export const key: string = "connorcC@12";
